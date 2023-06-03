473efe308ed846abf7091bc61d300777e4f1729a
Rhejoel Joson <rhejoelstrong@gmail.com>
2023-06-03 18:19:05 +0800

    minor revision

--- app/Http/Controllers/User/UserDashboardController.php
+++ app/Http/Controllers/User/UserDashboardController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\UserAssignedVehicle;
+use App\Models\Request_History;
 
 class UserDashboardController extends Controller
 {
