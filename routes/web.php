2d0f031f179728d37a1e68bcb054fe4af9329c99
Rhejoel Joson <rhejoelstrong@gmail.com>
2023-06-08 01:15:12 +0800

    minor additional

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::middleware(['auth'])->controller(AdminDashboardController::class)->group(
 
 Route::middleware(['auth'])->controller(AdminForApprovalController::class)->group(function () {
     Route::get('/admin/for-approval', "index");
+    
 });
 
 Route::middleware(['auth'])->controller(AdminHistoryController::class)->group(function () {
